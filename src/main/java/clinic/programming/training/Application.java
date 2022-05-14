9ad5673c80794ad80c54a412bcc9dea5f944576b
ylenia.gramuglia <yleg@live.it>
2022-05-14 19:50:37 +0200

    add commons lang3 dependecies

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,8 +2,14 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+	
+	public int countWords(String words){
+		String[] separateWords = StringUtils.split(words, ' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
 	
 	public void greet() {
 	
@@ -25,5 +31,9 @@ public class Application {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		
+		int count = app.countWords("I jave four words");
+		System.out.println("Word count: " + count);
+		
     }
 }
